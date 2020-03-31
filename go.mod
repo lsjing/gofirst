f70b0c4bc8c949907bc8270d9dee92633f99e333
xuef <xuefeng@limiketang.com>
2020-03-31 18:38:39 +0800

    feat,simple route

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lsjing/gofirst
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.2
+	github.com/go-ini/ini v1.55.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import "github.com/lsjing/gofirst/router"
-import "github.com/gin-gonic/gin"
 import "github.com/lsjing/gofirst/utils"
+import "github.com/gin-gonic/gin"
 
-func main(){
+func main() {
 	gin.SetMode(gin.DebugMode)
-
 	r := router.Setup()
-
 	port := utils.Config.Section("system").Key("http_port").String()
 	r.Run(":" + port)
 }
